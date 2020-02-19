be68ce5b11e9ac62a83dccefe8d814acc6198dfb
Michael Romay <amerror@gmail.com>
2020-02-19 11:07:10 -0800

    added weather

--- app.js
+++ app.js
@@ -1,51 +1,56 @@
 const express = require('express');
 const app = express();
-// const request = require('superagent');
-
-const darkSkyData = require('./data/darksky.json');
-
+// const weather = require('./data/darksky.json');
 const geoData = require('./data/geo.json');
-const geoParse = geoData[0];
-
-// console.log(geoData[0].display_name);
+const cors = require('cors');
 
 
-app.get('/location/:lat/:lon/', (req, res) => {
+app.use(cors);
+
+app.get('/location/', (req, res) => {
+    const cityData = geoData.results[0];
+    // console.log(req.query);
     res.json({
-        search_query: 'whatever',
-        formatted_query: 'other-thing',
-        location_name: geoParse.display_name,
-        lat: geoParse.lat,
-        lon: geoParse.lon
+        name: req.query.name,
+        formatted_query: cityData.formatted_address,
+        lat: cityData.geometry.location.lat,
+        lon: cityData.geometry.location.lon
     });
 });
 
-// console.log(darkSkyData.currently.summary);
-
-app.get('/weather/:lat/:lon/', (req, res) => {
-    res.json([
-        {
-            lat: darkSkyData.latitude,
-            lon: darkSkyData.longitude,
-            forecast: darkSkyData.currently.summary,
-            time: dateString
-        }
-    ]);
-});
+// const getWeatherData = (lat, lon) => {
+//     return weather.daily.data.map(forecast => {
+//         return {
+//             forecast: forecast.summary,
+//             time: new Date(forecast.time),
+//         };
+//     });
+// };
 
 
-let theDate = new Date(darkSkyData.currently.time * 1000);
-let dateString = theDate.toUTCString();
+// app.get('/weather', (req, res) => {
+//     // use the lat and lon from earlier to get the weather data for the selected area
+//     const portlandWeather = getWeatherData(lat, lon);
+//     // res.json that weather data in the appropriate form
+//     res.json([
+//         {
+//             res.json(portlandWeather)
+//             // lat: weather.latitude,
+//             // lon: weather.longitude,
+//             // forecast: weather.currently.summary,
+//             // time: dateString
+//         }
+//     ]);
+// });
 
-// console.log(dateString);
+// let theDate = new Date(weather.currently.time * 1000);
+// let dateString = theDate.toUTCString();
 
 app.get('*', (req, res) => {
     res.send({
         uhOh: '404'
     });
 });
-
-// app.listen(3000, () => { console.log('running . . .')});
 
 module.exports = {
     app: app,
