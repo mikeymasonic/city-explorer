b5d04971f022d27fd90951b4fb971d494d5dc0ed
Michael Romay <amerror@gmail.com>
2020-02-19 11:25:53 -0800

    herokutest

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
-// const weather = require('./data/darksky.json');
+const weather = require('./data/darksky.json');
 const geoData = require('./data/geo.json');
 const cors = require('cors');
 
+let lat;
+let lon;
 
-app.use(cors);
+app.use(cors());
+
+app.get('/', (request, respond) => respond.send('Hi!'));
 
 app.get('/location/', (req, res) => {
+    // ins www.cool-api.com?search=portland, `location` will be portland
+    const location = req.query.search;
+
+    console.log('using location . . .', location);
     const cityData = geoData.results[0];
+
+    lat = cityData.geometry.location.lat;
+    lon = cityData.geometry.location.lng;
     // console.log(req.query);
     res.json({
         name: req.query.name,
@@ -18,30 +29,22 @@ app.get('/location/', (req, res) => {
     });
 });
 
-// const getWeatherData = (lat, lon) => {
-//     return weather.daily.data.map(forecast => {
-//         return {
-//             forecast: forecast.summary,
-//             time: new Date(forecast.time),
-//         };
-//     });
-// };
+const getWeatherData = (lat, lon) => {
+    return weather.daily.data.map(forecast => {
+        return {
+            forecast: forecast.summary,
+            time: new Date(forecast.time * 1000),
+        };
+    });
+};
 
 
-// app.get('/weather', (req, res) => {
-//     // use the lat and lon from earlier to get the weather data for the selected area
-//     const portlandWeather = getWeatherData(lat, lon);
-//     // res.json that weather data in the appropriate form
-//     res.json([
-//         {
-//             res.json(portlandWeather)
-//             // lat: weather.latitude,
-//             // lon: weather.longitude,
-//             // forecast: weather.currently.summary,
-//             // time: dateString
-//         }
-//     ]);
-// });
+app.get('/weather', (req, res) => {
+    // use the lat and lon from earlier to get the weather data for the selected area
+    const portlandWeather = getWeatherData(lat, lon);
+    // res.json that weather data in the appropriate form
+    res.json(portlandWeather);
+});
 
 // let theDate = new Date(weather.currently.time * 1000);
 // let dateString = theDate.toUTCString();
