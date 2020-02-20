0cb82b0ae41165730cb1f313b95b472a847e61e4
Michael Romay <amerror@gmail.com>
2020-02-19 16:32:41 -0800

    weather api pulling

--- app.js
+++ app.js
@@ -1,36 +1,50 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const weather = require('./data/darksky.json');
-const geoData = require('./data/geo.json');
+// const weather = require('./data/darksky.json');
+// const geoData = require('./data/geo.json');
 const cors = require('cors');
-
-let lat;
-let lon;
+const request = require('superagent');
 
 app.use(cors());
 
-app.get('/', (request, respond) => respond.send('Hi!'));
+app.get('/', (req, res) => res.send('Hi!'));
 
-app.get('/location/', (req, res) => {
+// initialize the global state of lat and lng so it is accessible in other routes
+let lat;
+let lng;
+
+app.get('/location', async(req, res, next) => {
+    try {
     // ins www.cool-api.com?search=portland, `location` will be portland
-    const location = req.query.search;
+        const location = req.query.search;
+        const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${location}&format=json`;
 
-    console.log('using location . . .', location);
-    const cityData = geoData.results[0];
+        const cityData = await request.get(URL);
 
-    lat = cityData.geometry.location.lat;
-    lon = cityData.geometry.location.lng;
-    // console.log(req.query);
-    res.json({
-        name: req.query.name,
-        formatted_query: cityData.formatted_address,
-        lat: cityData.geometry.location.lat,
-        lon: cityData.geometry.location.lon
-    });
+        const firstResult = cityData.body[0];
+        
+        // console.log(locationData.body);
+        console.log('using location . . .', location);
+        // const cityData = geoData.results[0];
+
+        lat = firstResult.lat;
+        lng = firstResult.lon;
+        // console.log(req.query);
+        res.json({
+            formatted_query: firstResult.display_name,
+            lat: lat,
+            lng: lng
+        });
+    } catch (err) {
+        next(err);
+    }
 });
 
-const getWeatherData = (lat, lon) => {
-    return weather.daily.data.map(forecast => {
+const getWeatherData = async(lat, lng) => {
+    const weather = await request.get(`https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${lat},${lng}`);
+
+    return weather.body.daily.data.map(forecast => {
         return {
             forecast: forecast.summary,
             time: new Date(forecast.time * 1000),
@@ -39,11 +53,16 @@ const getWeatherData = (lat, lon) => {
 };
 
 
-app.get('/weather', (req, res) => {
+app.get('/weather', async(req, res) => {
     // use the lat and lon from earlier to get the weather data for the selected area
-    const portlandWeather = getWeatherData(lat, lon);
-    // res.json that weather data in the appropriate form
-    res.json(portlandWeather);
+    try {
+        const portlandWeather = await getWeatherData(lat, lng);
+
+        // res.json that weather data in the appropriate form
+        res.json(portlandWeather);
+    } catch (err) {
+        // next(err);
+    }
 });
 
 // let theDate = new Date(weather.currently.time * 1000);
